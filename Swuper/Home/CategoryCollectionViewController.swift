c4850ca991f8b00c54f8cac6b42e7a974d3eb0b5
jjoo0803 <jhjh083@naver.com>
2019-05-04 19:47:54 +0900

    홈화면 세그먼트 수정

--- Swuper/Home/CategoryCollectionViewController.swift
+++ Swuper/Home/CategoryCollectionViewController.swift
@@ -10,13 +10,42 @@ import UIKit
 
 class CategoryCollectionViewController: UIViewController {
 
+    @IBOutlet var categorySegment: UISegmentedControl!
+    
+    let buttonBar = UIView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        customSegment()
         // Do any additional setup after loading the view.
     }
     
 
+    func customSegment() {
+        categorySegment.backgroundColor = .clear
+        categorySegment.tintColor = .clear
+        categorySegment.setTitleTextAttributes([
+            NSAttributedString.Key.font : UIFont(name: "DINCondensed-Bold", size: 18),
+            NSAttributedString.Key.foregroundColor: UIColor.lightGray
+            ], for: .normal)
+        categorySegment.setTitleTextAttributes([
+            NSAttributedString.Key.font : UIFont(name: "DINCondensed-Bold", size: 18),
+            NSAttributedString.Key.foregroundColor: UIColor.orange
+            ], for: .selected)
+        buttonBar.translatesAutoresizingMaskIntoConstraints = false
+        buttonBar.backgroundColor = UIColor.orange
+        view.addSubview(buttonBar)
+        buttonBar.topAnchor.constraint(equalTo: categorySegment.bottomAnchor).isActive = true
+        buttonBar.heightAnchor.constraint(equalToConstant: 5).isActive = true
+        buttonBar.leftAnchor.constraint(equalTo: categorySegment.leftAnchor).isActive = true
+        buttonBar.widthAnchor.constraint(equalTo: categorySegment.widthAnchor, multiplier: 1 / CGFloat(categorySegment.numberOfSegments)).isActive = true
+    }
+    
+    @IBAction func categorySegmentValueChanged(_ sender: UISegmentedControl) {
+        UIView.animate(withDuration: 0.3) {
+            self.buttonBar.frame.origin.x = (self.categorySegment.frame.width / CGFloat(self.categorySegment.numberOfSegments)) * CGFloat(self.categorySegment.selectedSegmentIndex)
+        }
+    }
     /*
     // MARK: - Navigation
 
